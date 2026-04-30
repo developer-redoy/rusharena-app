cd0eb2ded46bc25e52bcaf8d1918d3a4bd894958
redoy <redoy.hossen.emon@gmail.com>
2026-05-01 00:45:29 +0600

    gmail by login

---
+++ capacitor.js
@@ -0,0 +1,16 @@
+/** @type {import('@capacitor/cli').CapacitorConfig} */
+const config = {
+  appId: "com.rusharena.app",
+  appName: "Rush Arena",
+  webDir: "out", // or .next depending on your build
+  plugins: {
+    GoogleAuth: {
+      scopes: ["profile", "email"],
+      serverClientId:
+        "564606184768-ddn0l8ddakps89pmqraiccsjfo2v5j82.apps.googleusercontent.com",
+      forceCodeForRefreshToken: true,
+    },
+  },
+};
+
+module.exports = config;
